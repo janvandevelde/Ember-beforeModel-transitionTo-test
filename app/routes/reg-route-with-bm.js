2d72a79a3f00aedab8d6326ebea74acc2d13c68b
Jan Van de Velde <jan.vndevelde@gmail.com>
2020-12-15 15:41:51 +0100

    Removing old test code

--- app/routes/reg-route-with-bm.js
+++ app/routes/reg-route-with-bm.js
@@ -20,15 +20,6 @@ export default class RegRouteWithBm extends Route {
     }
 
     if (!params.mandatoryQp) {
-      // TransitionTo doesn't seem to work in case of abort?
-      // transition.abort();
-
-      // followRedirects?
-      // https://github.com/emberjs/ember.js/blob/v3.22.0/packages/%40ember/-internals/routing/lib/system/transition.ts#L164
-
-      // queryParams tests:
-      // https://github.com/emberjs/ember.js/blob/58daefff26862e567f8a40d6e87df5a5dd828475/packages/ember/tests/routing/query_params_test.js
-
       console.log('doing transitionTo in route beforeModel');
       console.log('--------------');
 
